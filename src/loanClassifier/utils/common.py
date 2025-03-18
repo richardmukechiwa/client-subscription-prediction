1fc91117987e0dee60ac5e0d0ccad2b761ff811c
Richard <mukechiwarichard@gmail.com>
2025-03-18 21:56:14 +0200

    exception logger and utils added

--- src/loanClassifier/utils/common.py
+++ src/loanClassifier/utils/common.py
@@ -33,7 +33,7 @@ def read_yaml(path_to_yaml: Path) ->ConfigBox:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
-    except BoxValueError:
+    except BoxError:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
