7360c36d1915dc1a6c13fa5fac960cb4b8783573
Richard <mukechiwarichard@gmail.com>
2025-04-04 23:55:47 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
-#import setuptools
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
-    
+
 __version__ = "0.0.0"
 
 REPO_NAME = "client-subscription-prediction"
 AUTHOR_USER_NAME = "richardmukechiwa"
 SRC_REPO = "clientClassifier"
-AUTHOR_EMAIL  = "mukechiwarichard@gmail.com"
+AUTHOR_EMAIL = "mukechiwarichard@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A package to classify clients based on their subscription status",
+    description="A machine learning package to predict client subscription to term deposits",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"": "src"},   
-    packages=setuptools.find_packages(where="src")
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
 )
