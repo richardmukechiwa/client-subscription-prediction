4f25a773d143ce7c31c0c74ac51f73b42bb48c9a
Richard <mukechiwarichard@gmail.com>
2025-04-05 13:43:57 +0200

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from loanClassifier import logger
+from clientClassifier import logger
+from clientClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("Welcome to our custom logging")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f"{'>>'*20}stage {STAGE_NAME} started {'<<'*20}")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"{'>>'*20} stage {STAGE_NAME} completed {'<<'*20}")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- template.py
+++ template.py
@@ -31,7 +31,7 @@ list_of_files =[
     "research/trials.ipynb",
     "templates/index.html",
     "pyproject.toml",
-     "mlflow.py"
+    "mlflow.py"
     
     
 ]
