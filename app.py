48ef82d69db3b512941798946a4c4907bfc32a45
Richard <mukechiwarichard@gmail.com>
2025-05-01 23:36:37 +0200

    Prediction code update

--- app.py
+++ app.py
@@ -1,41 +1,51 @@
 from flask import Flask, render_template, request
+import pandas as pd
 import os
-import numpy as np
-import pandas as pd 
-from  clientClassifier.pipeline.prediction import PredictionPipeline
-
+from clientClassifier.pipeline.prediction import PredictionPipeline
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET']) # route to display the home page
+@app.route('/', methods=['GET'])
+def homePage():
+    return render_template('index.html')
+
+@app.route('/train', methods=['GET'])
+def training():
+    os.system('python main.py')
+    return 'Training successfully completed!'  
+
+    
+
+@app.route('/predict', methods=['GET', 'POST'])  # Allow both GET and POST
 def index():
     if request.method == 'POST':
         try:
             # Get the input data from the form
             age = int(request.form['age'])
-            month = int(request.form['month'])
+            month = str(request.form['month'])
             day = int(request.form['day'])
             balance = int(request.form['balance'])
             poutcome = str(request.form['poutcome'])
 
             # Create a DataFrame from the input data
-            data = pd.DataFrame([[age, month, day, balance, poutcome]], columns=['age', 'month', 'day', 'balance', 'poutcome'])
+            data = pd.DataFrame([[age, month, day, balance, poutcome]], 
+                                columns=['age', 'month', 'day', 'balance', 'poutcome'])
 
             # Load the prediction pipeline and make a prediction
             pipeline = PredictionPipeline()
-            prediction = pipeline.predict(data)
+            predict = pipeline.predict(data)
 
             # Render the result on the template
-            return render_template('index.html', prediction=prediction[0])
+            return render_template('results.html', prediction=str(predict))
+
         except Exception as e:  
-            return render_template('index.html', prediction="Error: " + str(e))
+            print("Error in prediction:", e)
+            return 'Something went wrong! Please check your input data.'
     else:
-        return render_template('index.html')        
+        return render_template('index.html')  # Render form on GET request
 
-       
-            
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
 
-if __name__  == "__main__":
-    app.run(host="0.0.0.0", port  = 8080)
     
